09b462b30bb1a40992c63a2cd2a934a994f840db
Sadman Wasee <sadman.wasee125@gmail.com>
2025-07-08 16:19:19 +0600

    added javascript files

---
+++ script.js
@@ -0,0 +1,57 @@
+const project1 = document.querySelector(".project-1");
+const project2 = document.querySelector(".project-2");
+const project3 = document.querySelector(".project-3");
+const project4 = document.querySelector(".project-4");
+
+const explore1 = document.querySelector(".explore1");
+const project_description1 = document.querySelector(".project-description1");
+
+const explore2 = document.querySelector(".explore2");
+const project_description2 = document.querySelector(".project-description2");
+
+const explore3 = document.querySelector(".explore3");
+const project_description3 = document.querySelector(".project-description3");
+
+const explore4 = document.querySelector(".explore4");
+const project_description4 = document.querySelector(".project-description4");
+
+project1.addEventListener("mouseover", ()=>{
+    project_description1.style.display = "none";
+    explore1.style.display = "block"; 
+})
+
+project1.addEventListener("mouseout", ()=>{
+    project_description1.style.display = "block";
+    explore1.style.display = "none"; 
+})
+
+project2.addEventListener("mouseover", ()=>{
+    project_description2.style.display = "none";
+    explore2.style.display = "block"; 
+})
+
+project2.addEventListener("mouseout", ()=>{
+    project_description2.style.display = "block";
+    explore2.style.display = "none"; 
+})
+
+
+project3.addEventListener("mouseover", ()=>{
+    project_description3.style.display = "none";
+    explore3.style.display = "block"; 
+})
+
+project3.addEventListener("mouseout", ()=>{
+    project_description3.style.display = "block";
+    explore3.style.display = "none"; 
+})
+
+project4.addEventListener("mouseover", ()=>{
+    project_description4.style.display = "none";
+    explore4.style.display = "block"; 
+})
+
+project4.addEventListener("mouseout", ()=>{
+    project_description4.style.display = "block";
+    explore4.style.display = "none"; 
+})
