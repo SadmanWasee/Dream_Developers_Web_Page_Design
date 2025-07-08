223ed36b0a435c94d9d0a78bf5a6a9f37ca6503b
Sadman Wasee <sadman.wasee125@gmail.com>
2025-07-08 09:01:35 +0600

    correction

--- script.js
+++
@@ -1,9 +0,0 @@
-let houses = document.querySelectorAll('img.house-images')
-
-houses.forEach(element => {
-
-    element.addEventListener("mouseover", ()=>{
-        element.textContent = "Explore"
-    })
-    
-});
