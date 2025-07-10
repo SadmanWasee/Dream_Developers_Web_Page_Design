f159026212c58c4db9eca89d05f94476beb06f56
Sadman Wasee <sadman.wasee125@gmail.com>
2025-07-10 09:06:34 +0600

    Used css to add on hover effect on project images instead of javascript. Made some code improvements.

--- script.js
+++ script.js
@@ -15,43 +15,56 @@ const project_description3 = document.querySelector(".project-description3");
 const explore4 = document.querySelector(".explore4");
 const project_description4 = document.querySelector(".project-description4");
 
-project1.addEventListener("mouseover", ()=>{
-    project_description1.style.display = "none";
-    explore1.style.display = "block"; 
-})
+// project1.addEventListener("mouseover", ()=>{
+//     project_description1.style.display = "none";
+//     explore1.style.display = "block"; 
+// })
 
-project1.addEventListener("mouseout", ()=>{
-    project_description1.style.display = "block";
-    explore1.style.display = "none"; 
-})
+// project1.addEventListener("mouseout", ()=>{
+//     project_description1.style.display = "block";
+//     explore1.style.display = "none"; 
+// })
 
-project2.addEventListener("mouseover", ()=>{
-    project_description2.style.display = "none";
-    explore2.style.display = "block"; 
-})
+// project2.addEventListener("mouseover", ()=>{
+//     project_description2.style.display = "none";
+//     explore2.style.display = "block"; 
+// })
 
-project2.addEventListener("mouseout", ()=>{
-    project_description2.style.display = "block";
-    explore2.style.display = "none"; 
-})
+// project2.addEventListener("mouseout", ()=>{
+//     project_description2.style.display = "block";
+//     explore2.style.display = "none"; 
+// })
 
 
-project3.addEventListener("mouseover", ()=>{
-    project_description3.style.display = "none";
-    explore3.style.display = "block"; 
-})
+// project3.addEventListener("mouseover", ()=>{
+//     project_description3.style.display = "none";
+//     explore3.style.display = "block"; 
+// })
 
-project3.addEventListener("mouseout", ()=>{
-    project_description3.style.display = "block";
-    explore3.style.display = "none"; 
-})
+// project3.addEventListener("mouseout", ()=>{
+//     project_description3.style.display = "block";
+//     explore3.style.display = "none"; 
+// })
 
-project4.addEventListener("mouseover", ()=>{
-    project_description4.style.display = "none";
-    explore4.style.display = "block"; 
-})
+// project4.addEventListener("mouseover", ()=>{
+//     project_description4.style.display = "none";
+//     explore4.style.display = "block"; 
+// })
 
-project4.addEventListener("mouseout", ()=>{
-    project_description4.style.display = "block";
-    explore4.style.display = "none"; 
-})
+// project4.addEventListener("mouseout", ()=>{
+//     project_description4.style.display = "block";
+//     explore4.style.display = "none"; 
+// })
+
+
+const facebook_icon = document.querySelector(".facebook-icons");
+const facebook_icon_white = document.querySelector(".facebook-icon-white")
+
+// facebook_icon.addEventListener("mouseover", ()=>{
+//     facebook_icon_white.style.display="none";
+// })
+
+// facebook_icon.addEventListener("mouseout", ()=>{
+//     facebook_icon_white.style.display="block";
+//     facebook_icon_white.style.top = "0"
+// })
